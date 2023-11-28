79e33c6a534553a7696167c6a5df63395c65ec33
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-28 08:08:02 +0600

    Create Skeleton Component: TestCardSkeleton
    This commit introduces a new reusable component for displaying skeletons of test cards. The TestCardSkeleton component uses the MUI Skeleton component to create a placeholder for loading content, providing a smooth loading experience for users

---
+++ src/components/skeleton/TestCardSkeleton.jsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "@mui/material";
+
+const TestCardSkeleton = () => {
+  return (
+    <div className="">
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={20} sx={{ mt: 1 }} />
+      <Skeleton variant="rectangular" height={20} width={200} sx={{ mt: 1 }} />
+      <Skeleton variant="rectangular" height={20} sx={{ mt: 1 }} />
+    </div>
+  );
+};
+
+export default TestCardSkeleton;
