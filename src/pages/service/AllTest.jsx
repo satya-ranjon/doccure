70f66821d369d4faf26586abe6afb7e466ffe3f4
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-28 17:47:01 +0600

    Create AllTest Component and TestCard Component
    - AllTest: This component displays a list of tests, allowing users to filter them by date. It uses the TestCard component to render individual test items. The filter functionality allows users to select a date and view tests available on that day.
    
    - TestCard: This component represents a single test card, displaying the test image, title, available days, and price. It also provides a 'Details' button to navigate to the detailed view of the test.

--- src/pages/service/AllTest.jsx
+++ src/pages/service/AllTest.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Chip, Container } from "@mui/material";
 import PageHeader from "../../components/common/PageHeader";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -7,8 +7,8 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useState } from "react";
 import SortIcon from "@mui/icons-material/Sort";
 import useAllTest from "../../hooks/useAllTest";
-import CloseIcon from "@mui/icons-material/Close";
 import TestCardSkeleton from "../../components/skeleton/TestCardSkeleton";
+import TestCard from "./TestCard";
 
 const days = [
   "Sunday",
@@ -30,23 +30,23 @@ const AllTest = () => {
     return dates.includes(selectedDay);
   });
 
-  const removeFilter = () => {
-    setFilterDate();
-  };
-
   let showData = filterDate ? tests : data;
 
+  const handleDelete = () => {
+    setFilterDate();
+  };
   return (
     <div className=" pb-28">
       <PageHeader>All / Tests</PageHeader>
       <Container sx={{ mt: 5 }}>
         <div className="text-sm mb-2 space-x-2">
-          <SortIcon /> <span>Filter</span>{" "}
+          <SortIcon /> <span>Filter</span>
           {filterDate && (
-            <span className=" px-2 py-1 text-xs bg-sky-600 text-white">
-              {days[selectedDay]}{" "}
-              <CloseIcon onClick={removeFilter} className=" cursor-pointer" />
-            </span>
+            <Chip
+              label={days[selectedDay]}
+              variant="outlined"
+              onDelete={handleDelete}
+            />
           )}
         </div>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -79,34 +79,12 @@ const AllTest = () => {
           {!loading && (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
               {showData.map((item) => (
-                <div className="shadow-2xl shadow-blue-500/20" key={item._id}>
-                  <img src={item.img} alt="" className=" h-48 w-full" />
-                  <Box py={1} p={2}>
-                    <h1 className="text-xl font-bold">{item.title}</h1>
-                    <div className="flex flex-wrap gap-2">
-                      <h1 className=" text-sm font-semibold">Available day:</h1>
-                      <Box>
-                        {item.availableDate.map((item) => (
-                          <span
-                            className="py-1 px-1 text-xs bg-sky-600 text-white"
-                            key={item}>
-                            {days[item].slice(0, 3)}
-                          </span>
-                        ))}
-                      </Box>
-                    </div>
-                    <Button sx={{ mt: 1 }} variant="contained">
-                      Detail
-                    </Button>
-                  </Box>
-                </div>
+                <TestCard key={item._id} item={item} />
               ))}
             </div>
           )}
         </Box>
       </Container>
-
-      <div className=" h-screen"></div>
     </div>
   );
 };
---
+++ src/pages/service/TestCard.jsx
@@ -0,0 +1,47 @@
+import { Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const TestCard = ({ item }) => {
+  return (
+    <div className="shadow-2xl shadow-blue-500/20" key={item?._id}>
+      <img src={item?.img} alt="" className=" h-48 w-full" />
+      <Box py={1} p={2}>
+        <h1 className="text-xl font-bold">{item?.title}</h1>
+        <div className="flex flex-wrap gap-2">
+          <h1 className=" text-sm font-semibold">Available day:</h1>
+          <Box>
+            {item?.availableDate.map((item) => (
+              <span
+                className="py-1 px-1 text-xs bg-sky-600 text-white"
+                key={item}>
+                {days[item].slice(0, 3)}
+              </span>
+            ))}
+          </Box>
+        </div>
+        <div className="flex mt-4 justify-between items-start gap-2">
+          <Button size="small" variant="outlined" disableTouchRipple>
+            <span className=" font-bold ">Price : {item?.price} ৳</span>
+          </Button>
+          <Link to={`/tests/${item?._id}`}>
+            <Button size="small" variant="contained">
+              <span className=" font-bold ">Details</span>
+            </Button>
+          </Link>
+        </div>
+      </Box>
+    </div>
+  );
+};
+
+export default TestCard;
