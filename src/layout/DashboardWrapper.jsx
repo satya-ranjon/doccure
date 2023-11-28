517fd002713a90c9a3dedd299a06a0fe993b2248
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-28 17:50:43 +0600

    Add DashboardWrapper component
    - Created a DashboardWrapper component that serves as a layout for the dashboard.
    - The component includes a responsive app bar, drawer, and user menu.
    - The app bar includes a logo and a menu icon for opening and closing the drawer.
    - The drawer contains navigation links and additional items.
    - Added media query support for handling responsiveness.
    - Used MUI components such as AppBar, Drawer, Toolbar, IconButton, Menu, MenuItem, Avatar, Tooltip, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography, and more.

---
+++ src/layout/DashboardWrapper.jsx
@@ -0,0 +1,224 @@
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { images } from "../constant";
+import { Avatar, Menu, MenuItem, Tooltip } from "@mui/material";
+import useAuthentication from "../hooks/useAuthentication";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import DatasetIcon from "@mui/icons-material/Dataset";
+
+const drawerWidth = 240;
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const navLink = [
+  {
+    icon: <DatasetIcon />,
+    label: "Hero Section",
+    link: "/dashboard/hero-section",
+  },
+  { icon: <PeopleAltIcon />, label: "Users", link: "/dashboard/users" },
+];
+
+const settings = [
+  { link: "/", label: "Home" },
+  { link: "/logout", label: "Logout" },
+];
+function DashboardWrapper() {
+  const theme = useTheme();
+  const matches = useMediaQuery("(min-width:1024px)");
+  const [open, setOpen] = React.useState(true);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { user, logoutUser } = useAuthentication();
+  const navigate = useNavigate();
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = (data) => {
+    if (data === "/logout") {
+      logoutUser();
+      return;
+    }
+    navigate(data);
+    setAnchorElUser(null);
+  };
+  React.useEffect(() => {
+    setOpen(matches);
+  }, [matches]);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar position="fixed" color="inherit" open={open}>
+        <div className="flex justify-between items-center">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography noWrap component="div">
+              <img src={images.footerLogo} alt="" className=" w-32 " />
+            </Typography>
+          </Toolbar>
+          <Box sx={{ flexGrow: 0, pr: 2 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src={user.photoURL} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}>
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting.link}
+                  onClick={() => handleCloseUserMenu(setting.link)}>
+                  <Typography textAlign="center">{setting.label}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </div>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {navLink.map((item, index) => (
+            <Link to={item.link} key={index}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.label} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={matches ? open : matches}>
+        <DrawerHeader />
+        <Outlet />
+      </Main>
+    </Box>
+  );
+}
+
+export default DashboardWrapper;
