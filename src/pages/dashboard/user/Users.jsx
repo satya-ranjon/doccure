1ab6bc077ddf7b3e827e3b360b5dc5f7686a66d2
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-30 21:02:07 +0600

    feat: Implemented Users component for managing user details
    In this commit:
    - Used MUI components such as Box, Button, Modal, NativeSelect, Skeleton, CircularProgress, LoadingButton, and icons like FmdGoodIcon, EmailIcon, AccessibilityIcon, SchoolIcon, MilitaryTechIcon
    - Fetched user data using useAllUser hook
    - Implemented user status and role change functionality with PUT requests
    - Added a modal to display detailed information about a selected user
    - Implemented pagination to fetch more user data
    - Enhanced the user interface with a responsive design

--- src/pages/dashboard/user/Users.jsx
+++ src/pages/dashboard/user/Users.jsx
@@ -9,6 +9,7 @@ import EmailIcon from "@mui/icons-material/Email";
 import AccessibilityIcon from "@mui/icons-material/Accessibility";
 import SchoolIcon from "@mui/icons-material/School";
 import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
+import DashboardContainer from "../../../components/common/DashboardContainer";
 const status = [
   { value: "active", label: "Active" },
   { value: "block", label: "Block" },
@@ -25,7 +26,6 @@ const Users = () => {
   const axios = useAxiosSecure();
   const [open, setOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState();
-  console.log(selectedUser);
 
   function handleClick() {
     if (data.length < totalUsers) {
@@ -73,157 +73,124 @@ const Users = () => {
   };
   return (
     <>
-      <div className=" max-w-[380px] md:max-w-full">
-        <div className="flex flex-wrap">
-          <div className="w-full ">
-            <div className="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white">
-              <div className="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
-                <div className="px-2 md:px-5 lg:px-9 pt-5 flex justify-between items-stretch flex-wrap min-h-[70px] pb-0 bg-transparent">
-                  <h3 className="flex flex-col items-start justify-center m-2 ml-0 font-medium text-xl/tight text-dark">
-                    <span className="mr-3 font-semibold text-dark">
-                      Total Users {totalUsers}
-                    </span>
-                  </h3>
-                </div>
-                {/* <!-- end card header --> */}
-                {/* <!-- card body  --> */}
-                <div className="flex-auto block py-8 pt-6 px-2 md:px-5 lg:px-9">
-                  <div className="overflow-x-auto">
-                    {loading ? (
-                      <>
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                      </>
-                    ) : (
-                      <table className="w-full  my-0 align-middle text-dark border-neutral-200">
-                        <thead className="align-bottom">
-                          <tr className="font-semibold text-[0.95rem] text-secondary-dark">
-                            <th className="pb-3 text-start ">Avatar</th>
-                            <th className="pb-3 text-start ">Name</th>
-                            <th className="pb-3 text-start ">Email</th>
-                            <th className="pb-3 text-start ">Status</th>
-                            <th className="pb-3 text-start ">Role</th>
-                            <th className="pb-3 text-start ">Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {!loading &&
-                            data.map((item) => {
-                              const activeStatus = item.status;
-                              const activeRole = item.role;
-                              return (
-                                <tr
-                                  key={item._id}
-                                  className="border-b border-dashed last:border-b-0">
-                                  <td className="p-3 pl-0">
-                                    <img
-                                      src={item.avatar}
-                                      className="w-[50px] h-[50px] inline-block shrink-0 rounded-2xl"
-                                      alt=""
-                                    />
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.name}
-                                    </span>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.email}
-                                    </span>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <NativeSelect
-                                      defaultValue={activeStatus}
-                                      onChange={(event) =>
-                                        handleStatusChange(
-                                          event.target.value,
-                                          item._id,
-                                          item.name
-                                        )
-                                      }
-                                      inputProps={{
-                                        name: "status",
-                                        id: "uncontrolled-native",
-                                      }}>
-                                      {status.map((item) => (
-                                        <option
-                                          key={item.value}
-                                          value={item.value}>
-                                          {item.label}
-                                        </option>
-                                      ))}
-                                    </NativeSelect>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <NativeSelect
-                                      defaultValue={activeRole}
-                                      onChange={(event) =>
-                                        handleRoleChange(
-                                          event.target.value,
-                                          item._id,
-                                          item.name
-                                        )
-                                      }
-                                      inputProps={{
-                                        name: "role",
-                                        id: "uncontrolled-native",
-                                      }}>
-                                      {roles.map((item) => (
-                                        <option
-                                          key={item.value}
-                                          value={item.value}>
-                                          {item.label}
-                                        </option>
-                                      ))}
-                                    </NativeSelect>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <Button
-                                      onClick={() => handleOpen(item)}
-                                      variant="outlined"
-                                      size="small">
-                                      Details
-                                    </Button>
-                                  </td>
-                                </tr>
-                              );
-                            })}
-                        </tbody>
-                      </table>
-                    )}
-                  </div>
-                </div>
-              </div>
-              {data.length < totalUsers && !loading && (
-                <LoadingButton
-                  size="small"
-                  sx={{ mt: 5 }}
-                  onClick={handleClick}
-                  loading={fetchMoreLoading}
-                  loadingIndicator="Loading…"
-                  variant="outlined">
-                  <span>Fetch more data</span>
-                </LoadingButton>
-              )}
-            </div>
-          </div>
+      <DashboardContainer label={` TOTAL USERS ${totalUsers}`}>
+        <div className="max-w-[300px] md:max-w-full overflow-scroll md:overflow-hidden">
+          {loading ? (
+            <>
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+            </>
+          ) : (
+            <table className=" min-w-[900px] md:min-w-full my-0 align-middle text-dark border-neutral-200">
+              <thead className="align-bottom">
+                <tr className="font-semibold text-[0.95rem] text-secondary-dark">
+                  <th className="pb-3 text-start ">Avatar</th>
+                  <th className="pb-3 text-start ">Name</th>
+                  <th className="pb-3 text-start ">Email</th>
+                  <th className="pb-3 text-start ">Status</th>
+                  <th className="pb-3 text-start ">Role</th>
+                  <th className="pb-3 text-start ">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!loading &&
+                  data.map((item) => {
+                    const activeStatus = item.status;
+                    const activeRole = item.role;
+                    return (
+                      <tr
+                        key={item._id}
+                        className="border-b border-dashed last:border-b-0">
+                        <td className="p-3 pl-0">
+                          <img
+                            src={item.avatar}
+                            className="w-[50px] h-[50px] inline-block shrink-0 rounded-2xl"
+                            alt=""
+                          />
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.name}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.email}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <NativeSelect
+                            defaultValue={activeStatus}
+                            onChange={(event) =>
+                              handleStatusChange(
+                                event.target.value,
+                                item._id,
+                                item.name
+                              )
+                            }
+                            inputProps={{
+                              name: "status",
+                              id: "uncontrolled-native",
+                            }}>
+                            {status.map((item) => (
+                              <option key={item.value} value={item.value}>
+                                {item.label}
+                              </option>
+                            ))}
+                          </NativeSelect>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <NativeSelect
+                            defaultValue={activeRole}
+                            onChange={(event) =>
+                              handleRoleChange(
+                                event.target.value,
+                                item._id,
+                                item.name
+                              )
+                            }
+                            inputProps={{
+                              name: "role",
+                              id: "uncontrolled-native",
+                            }}>
+                            {roles.map((item) => (
+                              <option key={item.value} value={item.value}>
+                                {item.label}
+                              </option>
+                            ))}
+                          </NativeSelect>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <Button
+                            onClick={() => handleOpen(item)}
+                            variant="outlined"
+                            size="small">
+                            Details
+                          </Button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          )}
+          {data.length < totalUsers && !loading && (
+            <LoadingButton
+              size="small"
+              fullWidth
+              sx={{ mt: 5 }}
+              onClick={handleClick}
+              loading={fetchMoreLoading}
+              loadingIndicator="Loading…"
+              variant="outlined">
+              <span>Fetch more data</span>
+            </LoadingButton>
+          )}
         </div>
-      </div>
-
+      </DashboardContainer>
       <Modal
         open={open}
         onClose={handleClose}
