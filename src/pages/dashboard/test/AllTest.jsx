65825c39e8066ba744788124e5c58f0ebfaf046b
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-30 21:00:11 +0600

    feat: Implemented AllTests management in AllTests component
    In this commit:
    - Created a page to manage all tests with a list of test details
    - Used MUI components such as Skeleton, Tooltip, IconButton
    - Utilized dayjs library for date formatting
    - Implemented deletion of tests with a confirmation dialog using SweetAlert2
    - Enabled navigation to the edit page for each test
    - Displayed loading indicators during data fetching
    - Improved user interface with conditional rendering based on loading state
    - Ensured responsive design for various screen sizes

--- src/pages/dashboard/test/AllTest.jsx
+++ src/pages/dashboard/test/AllTest.jsx
@@ -5,6 +5,8 @@ import useAllTest from "../../../hooks/useAllTest";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import DashboardContainer from "../../../components/common/DashboardContainer";
 
 const AllTests = () => {
   const axios = useAxiosSecure();
@@ -12,133 +14,113 @@ const AllTests = () => {
   const { data: AllTest, removeDataById, loading } = useAllTest();
 
   const handleDeleteTest = (id) => {
-    axios
-      .delete(`/tests/${id}`)
-      .then((res) => {
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: `Delete Test Successfully`,
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        removeDataById(id);
-      })
-      .catch((err) => console.log(err));
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(`/tests/${id}`)
+          .then((res) => {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Test has been deleted.",
+              icon: "success",
+            });
+            removeDataById(id);
+          })
+          .catch((err) => console.log(err));
+      }
+    });
   };
 
   return (
-    <>
-      <div className=" max-w-[380px] md:max-w-full">
-        <div className="flex flex-wrap">
-          <div className="w-full ">
-            <div className="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white">
-              <div className="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
-                <div className="px-2 md:px-5 lg:px-9 pt-5 flex justify-between items-stretch flex-wrap min-h-[70px] pb-0 bg-transparent">
-                  <h3 className="flex flex-col items-start justify-center m-2 ml-0 font-medium text-xl/tight text-dark">
-                    <span className="mr-3 font-semibold text-dark">
-                      ALL Tests
-                    </span>
-                  </h3>
-                </div>
-                {/* <!-- end card header --> */}
-                {/* <!-- card body  --> */}
-                <div className="flex-auto block py-8 pt-6 px-2 md:px-5 lg:px-9">
-                  <div className="overflow-x-auto">
-                    {loading ? (
-                      <>
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                      </>
-                    ) : (
-                      <table className="w-full  my-0 align-middle text-dark border-neutral-200">
-                        <thead className="align-bottom">
-                          <tr className="font-semibold text-[0.95rem] text-secondary-dark">
-                            <th className="pb-3 text-start ">Title</th>
-                            <th className="pb-3 text-start ">Price</th>
-                            <th className="pb-3 text-start ">Actions</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {!loading &&
-                            AllTest.map((item) => {
-                              return (
-                                <tr
-                                  key={item._id}
-                                  className="border-b border-dashed last:border-b-0">
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.title.slice(0, 35)}
-                                      {item.title.length > 35 && "..."}
-                                    </span>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.price}
-                                    </span>
-                                  </td>
+    <div>
+      <DashboardContainer label="ALL TEST">
+        <div className="max-w-[380px] md:max-w-full overflow-scroll md:overflow-hidden">
+          {loading ? (
+            <>
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+            </>
+          ) : (
+            <table className=" min-w-[700px] md:min-w-full w-full  my-0 align-middle text-dark border-neutral-200">
+              <thead className="align-bottom">
+                <tr className="font-semibold text-[0.95rem] text-secondary-dark">
+                  <th className="pb-3 text-start ">Title</th>
+                  <th className="pb-3 text-start ">Slots</th>
+                  <th className="pb-3 text-start ">Price</th>
+                  <th className="pb-3 text-start ">Date</th>
+                  <th className="pb-3 text-start ">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!loading &&
+                  AllTest.map((item) => {
+                    return (
+                      <tr
+                        key={item._id}
+                        className="border-b border-dashed last:border-b-0">
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.title.slice(0, 35)}
+                            {item.title.length > 35 && "..."}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.slots}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.price}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {dayjs(item?.availableDate).format("DD-MM-YYYY")}
+                          </span>
+                        </td>
 
-                                  <td className="p-3 pl-0 text-start">
-                                    <Tooltip placement="left" title="Edit">
-                                      <Link
-                                        to={`/dashboard/edit-test/${item._id}`}>
-                                        <IconButton
-                                          color="info"
-                                          aria-label="Edit"
-                                          size="large">
-                                          <EditIcon />
-                                        </IconButton>
-                                      </Link>
-                                    </Tooltip>
-                                    <Tooltip placement="right" title="Delete">
-                                      <IconButton
-                                        onClick={() =>
-                                          handleDeleteTest(item._id)
-                                        }
-                                        color="error"
-                                        aria-label="delete"
-                                        size="large">
-                                        <DeleteIcon />
-                                      </IconButton>
-                                    </Tooltip>
-                                  </td>
-                                </tr>
-                              );
-                            })}
-                        </tbody>
-                      </table>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                        <td className="p-3 pl-0 text-start">
+                          <Tooltip placement="left" title="Edit">
+                            <Link to={`/dashboard/edit-test/${item._id}`}>
+                              <IconButton
+                                color="info"
+                                aria-label="Edit"
+                                size="large">
+                                <EditIcon />
+                              </IconButton>
+                            </Link>
+                          </Tooltip>
+                          <Tooltip placement="right" title="Delete">
+                            <IconButton
+                              onClick={() => handleDeleteTest(item._id)}
+                              color="error"
+                              aria-label="delete"
+                              size="large">
+                              <DeleteIcon />
+                            </IconButton>
+                          </Tooltip>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          )}
         </div>
-      </div>
-    </>
+      </DashboardContainer>
+    </div>
   );
 };
 
