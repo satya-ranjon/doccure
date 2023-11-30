d2992fe4f71ed6e91e088caf8ad6eb665ba06988
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-30 23:13:37 +0600

    feat: Updated AllTest component
    In this commit:
    - Refactored the component to fetch paginated test data using axios and implemented pagination functionality.
    - Used the 'useAxiosPublic' hook for making public API requests.
    - Added pagination UI at the bottom of the test grid.
    - Adjusted the logic to handle pagination events and update the displayed test data accordingly.
    - Introduced loading and error states to enhance user experience during data fetching.
    - Minor style adjustments and improvements.

--- src/pages/service/AllTest.jsx
+++ src/pages/service/AllTest.jsx
@@ -1,24 +1,48 @@
-import { Box, Chip, Container } from "@mui/material";
+import { Box, Chip, Container, Pagination, Stack } from "@mui/material";
 import PageHeader from "../../components/common/PageHeader";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SortIcon from "@mui/icons-material/Sort";
-import useAllTest from "../../hooks/useAllTest";
 import TestCardSkeleton from "../../components/skeleton/TestCardSkeleton";
 import TestCard from "./TestCard";
 import dayjs from "dayjs";
 import DataNotFound from "../../components/common/DataNotFound";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const AllTest = () => {
   const [filterDate, setFilterDate] = useState(null);
   let selectedDay = filterDate ? new Date(filterDate) : new Date();
 
-  const { data, loading } = useAllTest();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [totalTest, setTotalTest] = useState([]);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
 
-  const tests = data.filter(
+  const axios = useAxiosPublic();
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    axios
+      .get(`/userTests?page=${page}`)
+      .then((res) => {
+        setTotalTest(res.data.totalTest);
+        setData(res.data.data);
+        setLoading(false);
+        setError(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      });
+  }, [page]);
+
+  const tests = data?.filter(
     (item) => item.availableDate === selectedDay?.toISOString()
   );
 
@@ -27,6 +51,12 @@ const AllTest = () => {
   const handleDelete = () => {
     setFilterDate(null);
   };
+
+  const handlePagination = (event, page) => {
+    setPage(page);
+  };
+
+  const count = Math.ceil(totalTest / 6);
   return (
     <div className=" pb-28">
       <PageHeader>All / Tests</PageHeader>
@@ -61,18 +91,34 @@ const AllTest = () => {
             </div>
           )}
 
-          {!loading && showData.length === 0 && (
+          {!loading && showData?.length === 0 && (
             <DataNotFound> Test Not Found</DataNotFound>
           )}
 
           {!loading && (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-              {showData.map((item) => (
+              {showData?.map((item) => (
                 <TestCard key={item._id} item={item} />
               ))}
             </div>
           )}
         </Box>
+        <Stack
+          spacing={2}
+          mt={5}
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+            width: "100%",
+            justifyItems: "end",
+          }}>
+          <Pagination
+            size="large"
+            count={count}
+            defaultPage={1}
+            onChange={handlePagination}
+          />
+        </Stack>
       </Container>
     </div>
   );
