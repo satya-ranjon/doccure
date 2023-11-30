392ac3e8fc0e4dd22c32c1db3732d17a9add079e
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-30 20:56:05 +0600

    feat: Implemented AddBanner component for adding banners
    In this commit:
    - Implemented a form using MUI TextField and Button for adding banners
    - Utilized useFormik for form management
    - Integrated useAxiosSecure for secure API requests
    - Displayed success message using Swal.fire upon successful banner creation

--- src/pages/dashboard/addBanner/AllBanner.jsx
+++ src/pages/dashboard/addBanner/AllBanner.jsx
@@ -5,6 +5,7 @@ import useAllBanner from "../../../hooks/useAllBanner";
 import { Link } from "react-router-dom";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import DashboardContainer from "../../../components/common/DashboardContainer";
 
 const isActive = [
   { value: "true", label: "True" },
@@ -31,166 +32,135 @@ const AllBanner = () => {
   };
 
   const handleDeleteBanner = (id) => {
-    axios
-      .delete(`/banner/${id}`)
-      .then((res) => {
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: `Delete Banner Successfully`,
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        removeDataById(id);
-      })
-      .catch((err) => console.log(err));
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(`/banner/${id}`)
+          .then((res) => {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Banner has been deleted.",
+              icon: "success",
+            });
+            removeDataById(id);
+          })
+          .catch((err) => console.log(err));
+      }
+    });
   };
 
   return (
     <>
-      <div className=" max-w-[380px] md:max-w-full">
-        <div className="flex flex-wrap">
-          <div className="w-full ">
-            <div className="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white">
-              <div className="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
-                <div className="px-2 md:px-5 lg:px-9 pt-5 flex justify-between items-stretch flex-wrap min-h-[70px] pb-0 bg-transparent">
-                  <h3 className="flex flex-col items-start justify-center m-2 ml-0 font-medium text-xl/tight text-dark">
-                    <span className=" text-2xl font-bold">ALL BANNERS</span>
-                  </h3>
-                </div>
-                {/* <!-- end card header --> */}
-                {/* <!-- card body  --> */}
-                <div className="flex-auto block py-8 pt-6 px-2 md:px-5 lg:px-9">
-                  <div className="overflow-x-auto">
-                    {loading ? (
-                      <>
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                        <Skeleton
-                          sx={{ mt: 2 }}
-                          variant="rectangular"
-                          height={35}
-                        />
-                      </>
-                    ) : (
-                      <table className="w-full  my-0 align-middle text-dark border-neutral-200">
-                        <thead className="align-bottom">
-                          <tr className="font-semibold text-[0.95rem] text-secondary-dark">
-                            <th className="pb-3 text-start ">Name</th>
-                            <th className="pb-3 text-start ">Title</th>
-                            <th className="pb-3 text-start ">Coupon Code</th>
-                            <th className="pb-3 text-start ">Coupon Rate</th>
-                            <th className="pb-3 text-start ">IsActive</th>
-                            <th className="pb-3 text-start ">Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {!loading &&
-                            data.map((item) => {
-                              return (
-                                <tr
-                                  key={item._id}
-                                  className="border-b border-dashed last:border-b-0">
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.name}
-                                    </span>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.title}
-                                    </span>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.couponCode}
-                                    </span>
-                                  </td>
-                                  <td className="p-3 pl-0 text-start">
-                                    <span className="font-semibold text-light-inverse text-md/normal">
-                                      {item.couponRate} %
-                                    </span>
-                                  </td>
+      <DashboardContainer label="ALL BANNERS">
+        <div className="max-w-[300px] md:max-w-[620px] lg:max-w-full overflow-scroll lg:overflow-hidden">
+          {loading ? (
+            <>
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+              <Skeleton sx={{ mt: 2 }} variant="rectangular" height={35} />
+            </>
+          ) : (
+            <table className="min-w-[800px] lg:min-w-full w-full  my-0 align-middle text-dark border-neutral-200">
+              <thead className="align-bottom">
+                <tr className="font-semibold text-[0.95rem] text-secondary-dark">
+                  <th className="pb-3 text-start ">Name</th>
+                  <th className="pb-3 text-start ">Title</th>
+                  <th className="pb-3 text-start ">Coupon Code</th>
+                  <th className="pb-3 text-start ">Coupon Rate</th>
+                  <th className="pb-3 text-start ">IsActive</th>
+                  <th className="pb-3 text-start ">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!loading &&
+                  data.map((item) => {
+                    return (
+                      <tr
+                        key={item._id}
+                        className="border-b border-dashed last:border-b-0">
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.name}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.title.slice(0, 20)}
+                            {item.title.length > 20 && "..."}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.couponCode}
+                          </span>
+                        </td>
+                        <td className="p-3 pl-0 text-start">
+                          <span className="font-semibold text-light-inverse text-md/normal">
+                            {item.couponRate} %
+                          </span>
+                        </td>
 
-                                  <td className="p-3 pl-0 text-start">
-                                    <NativeSelect
-                                      defaultValue={item.isActive}
-                                      onChange={(event) =>
-                                        handleStatusChange(
-                                          event.target.value,
-                                          item._id,
-                                          item.name
-                                        )
-                                      }
-                                      inputProps={{
-                                        name: "status",
-                                        id: "uncontrolled-native",
-                                      }}>
-                                      {isActive.map((item) => (
-                                        <option
-                                          key={item.value}
-                                          value={item.value}>
-                                          {item.label}
-                                        </option>
-                                      ))}
-                                    </NativeSelect>
-                                  </td>
+                        <td className="p-3 pl-0 text-start">
+                          <NativeSelect
+                            defaultValue={item.isActive}
+                            onChange={(event) =>
+                              handleStatusChange(
+                                event.target.value,
+                                item._id,
+                                item.name
+                              )
+                            }
+                            inputProps={{
+                              name: "status",
+                              id: "uncontrolled-native",
+                            }}>
+                            {isActive.map((item) => (
+                              <option key={item.value} value={item.value}>
+                                {item.label}
+                              </option>
+                            ))}
+                          </NativeSelect>
+                        </td>
 
-                                  <td className="p-3 pl-0 text-start">
-                                    <Tooltip placement="left" title="Edit">
-                                      <Link
-                                        to={`/dashboard/edit-banner/${item._id}`}>
-                                        <IconButton
-                                          color="info"
-                                          aria-label="Edit"
-                                          size="large">
-                                          <EditIcon />
-                                        </IconButton>
-                                      </Link>
-                                    </Tooltip>
-                                    <Tooltip placement="right" title="Delete">
-                                      <IconButton
-                                        onClick={() =>
-                                          handleDeleteBanner(item._id)
-                                        }
-                                        color="error"
-                                        aria-label="delete"
-                                        size="large">
-                                        <DeleteIcon />
-                                      </IconButton>
-                                    </Tooltip>
-                                  </td>
-                                </tr>
-                              );
-                            })}
-                        </tbody>
-                      </table>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                        <td className="p-3 pl-0 text-start">
+                          <Tooltip placement="left" title="Edit">
+                            <Link to={`/dashboard/edit-banner/${item._id}`}>
+                              <IconButton
+                                color="info"
+                                aria-label="Edit"
+                                size="large">
+                                <EditIcon />
+                              </IconButton>
+                            </Link>
+                          </Tooltip>
+                          <Tooltip placement="right" title="Delete">
+                            <IconButton
+                              onClick={() => handleDeleteBanner(item._id)}
+                              color="error"
+                              aria-label="delete"
+                              size="large">
+                              <DeleteIcon />
+                            </IconButton>
+                          </Tooltip>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          )}
         </div>
-      </div>
+      </DashboardContainer>
     </>
   );
 };
--- src/pages/dashboard/addBanner/UpdateBanner.jsx
+++ src/pages/dashboard/addBanner/UpdateBanner.jsx
@@ -5,6 +5,7 @@ import Swal from "sweetalert2";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useBannerById from "../../../hooks/useBannerById";
+import DashboardContainer from "../../../components/common/DashboardContainer";
 
 const validate = (values) => {
   const errors = {};
@@ -78,84 +79,85 @@ const UpdateBanner = () => {
   }, [loading, data]);
 
   return (
-    <div>
-      <h1 className=" text-2xl font-bold mt-8">ADD BANNER</h1>
-      {loading && (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            height: "600px",
-            alignItems: "center",
-          }}>
-          <CircularProgress size={180} />
-        </Box>
-      )}
-      {!loading && (
-        <form
-          onSubmit={formik.handleSubmit}
-          className=" flex flex-col gap-5 mt-5">
-          <TextField
-            value={formik.values.name}
-            error={formik.errors.name ? true : false}
-            label={formik.errors.name || "Name"}
-            onChange={formik.handleChange}
-            name="name"
-            id="outlined-basic"
-            variant="outlined"
-          />
-          <TextField
-            value={formik.values.title}
-            error={formik.errors.title ? true : false}
-            label={formik.errors.title || "Title"}
-            onChange={formik.handleChange}
-            name="title"
-            id="outlined-basic"
-            variant="outlined"
-          />
-          <TextField
-            value={formik.values.description}
-            error={formik.errors.description ? true : false}
-            label={formik.errors.description || "Description"}
-            onChange={formik.handleChange}
-            name="description"
-            id="outlined-basic"
-            variant="outlined"
-          />
-          <TextField
-            value={formik.values.imageUrl}
-            error={formik.errors.imageUrl ? true : false}
-            label={formik.errors.imageUrl || "Image Url"}
-            onChange={formik.handleChange}
-            name="imageUrl"
-            id="outlined-basic"
-            variant="outlined"
-          />
-          <TextField
-            value={formik.values.couponCode}
-            error={formik.errors.couponCode ? true : false}
-            label={formik.errors.couponCode || "Coupon Code"}
-            onChange={formik.handleChange}
-            name="couponCode"
-            id="outlined-basic"
-            variant="outlined"
-          />
-          <TextField
-            value={formik.values.couponRate}
-            error={formik.errors.couponRate ? true : false}
-            label={formik.errors.couponRate || "Coupon Rate"}
-            onChange={formik.handleChange}
-            name="couponRate"
-            id="outlined-basic"
-            variant="outlined"
-            type="number"
-          />
-          <Button type="submit" size="large" variant="contained">
-            Update Banner
-          </Button>
-        </form>
-      )}
-    </div>
+    <>
+      <DashboardContainer label="UPDATE BANNER">
+        {loading && (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              height: "600px",
+              alignItems: "center",
+            }}>
+            <CircularProgress size={180} />
+          </Box>
+        )}
+        {!loading && (
+          <form
+            onSubmit={formik.handleSubmit}
+            className=" flex flex-col gap-5 mt-5">
+            <TextField
+              value={formik.values.name}
+              error={formik.errors.name ? true : false}
+              label={formik.errors.name || "Name"}
+              onChange={formik.handleChange}
+              name="name"
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              value={formik.values.title}
+              error={formik.errors.title ? true : false}
+              label={formik.errors.title || "Title"}
+              onChange={formik.handleChange}
+              name="title"
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              value={formik.values.description}
+              error={formik.errors.description ? true : false}
+              label={formik.errors.description || "Description"}
+              onChange={formik.handleChange}
+              name="description"
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              value={formik.values.imageUrl}
+              error={formik.errors.imageUrl ? true : false}
+              label={formik.errors.imageUrl || "Image Url"}
+              onChange={formik.handleChange}
+              name="imageUrl"
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              value={formik.values.couponCode}
+              error={formik.errors.couponCode ? true : false}
+              label={formik.errors.couponCode || "Coupon Code"}
+              onChange={formik.handleChange}
+              name="couponCode"
+              id="outlined-basic"
+              variant="outlined"
+            />
+            <TextField
+              value={formik.values.couponRate}
+              error={formik.errors.couponRate ? true : false}
+              label={formik.errors.couponRate || "Coupon Rate"}
+              onChange={formik.handleChange}
+              name="couponRate"
+              id="outlined-basic"
+              variant="outlined"
+              type="number"
+            />
+            <Button type="submit" size="large" variant="contained">
+              Update Banner
+            </Button>
+          </form>
+        )}
+      </DashboardContainer>
+    </>
   );
 };
 
